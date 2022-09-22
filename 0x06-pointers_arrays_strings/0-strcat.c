76b4061c05daf9f6ea220943010770394f8338e2
williamsogbonna <ogbonnawilliams73@gmail.com>
2022-09-22 11:47:24 -0700

    my commit

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,36 @@
+ #include "main.h"
+
+ /**
+  * _strcat - concatenates one string to another
+  *
+  * @dest: destination string
+  *
+  * @src: String to be concatenated
+  *
+  * Return: The destination string
+  */
+
+char *_strcat(char *dest, char *src)
+
+{
+
+	int i;
+
+	int destination = 0;
+
+	int counter = 0;
+
+	while (dest[counter] != '\0')
+	{
+		destLenth++;
+
+		counter++;
+	}
+	for (i = 0; src[i] != '\0'; i++)
+	dest[destLength + i] = src[i];
+
+	dest[destLenght + i] = '\0';
+
+	return (dest);
+
+
